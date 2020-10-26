8b3635c7de9d95432a09b2a0785850588d4e5d0e
felipefeleol <felima.oleveiira@gmail.com>
2020-10-26 08:05:26 -0400

    Upload Files

--- app/src/main/java/com/example/spotifykirk3bi/MainActivity.java
+++ app/src/main/java/com/example/spotifykirk3bi/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.spotifykirk3bi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,7 +14,8 @@ import com.bumptech.glide.Glide;
 public class MainActivity extends AppCompatActivity {
 
     ImageView ivMusic;
-    TextView tvMusicName;
+    Button btnPlay;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,13 @@ public class MainActivity extends AppCompatActivity {
         ivMusic = (ImageView) findViewById(R.id.ivMusic);
         tvMusicName = (TextView) findViewById(R.id.tvMusicName);
         Glide.with(this).load("https://img.youtube.com/vi/tc-bxi26GSA/0.jpg").into(ivMusic);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.talarica);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer.start();
     }
 }
